61baae09224e491be9d38a299c22d97416f5f9ed
Anis Hamouda <anis.c.hamouda@gmail.com>
2023-05-30 08:43:50 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_instance" "ppp_ec2" {
             "sudo yum install -y yum-utils",
             "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
             "sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-            "sudo systemctl start docker
+            "sudo systemctl start docker"
         ]
     }
 
